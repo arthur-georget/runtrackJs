33fdbc7889012fef717ae4d366fe3bc825e3e91e
arthur-georget <arthur.georget@laplateforme.io>
2026-04-20 12:02:32 +0200

    feat(job05): job05 done.

---
+++ jour01/job05/script.js
@@ -0,0 +1,15 @@
+let joursSemaines = ["Lundi",
+                     "Mardi",
+                     "Mercredi",
+                     "Jeudi",
+                     "Vendredi",
+                     "Samedi",
+                     "Dimanche"]
+
+function afficherJoursSemaines(){
+    for (jour of joursSemaines){
+        console.log(jour);
+    }
+}
+
+afficherJoursSemaines();
