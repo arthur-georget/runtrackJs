f3f09b1c0cc1514d45b4ef4900da45c2981fbad6
arthur-georget <arthur.georget@laplateforme.io>
2026-04-23 15:53:01 +0200

    refactor(jour04): refactored job05 js script to handle API requests error correctly.

--- jour04/job05/script.js
+++ jour04/job05/script.js
@@ -5,18 +5,25 @@ async function loadCitation(){
     }
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const targetUrl = 'https://zenquotes.io/api/random';
-
-    let response = await fetch(proxyUrl + targetUrl);
-    let data = await response.json();
-    console.log(data);
-    const paragraph = document.createElement("p");
-    const paragraphContent = document.createTextNode(`Quote: ${data[0]["q"]}`);
-    const author = document.createElement("p");
-    const authorContent = document.createTextNode(`Author: ${data[0]["a"]}`);
-    paragraph.appendChild(paragraphContent);
-    author.appendChild(authorContent);
-    citationContainer.appendChild(paragraph);
-    citationContainer.appendChild(author);
+    try {
+        const response = await fetch(proxyUrl + targetUrl);
+        if (!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+        const paragraph = document.createElement("p");
+        const paragraphContent = document.createTextNode(`Quote: ${data[0]["q"]}`);
+        const author = document.createElement("p");
+        const authorContent = document.createTextNode(`Author: ${data[0]["a"]}`);
+        paragraph.appendChild(paragraphContent);
+        author.appendChild(authorContent);
+        citationContainer.appendChild(paragraph);
+        citationContainer.appendChild(author);
+    } catch (error) {
+        console.error(error.message);
+    }
+    
 }
 
 document.getElementById("button").addEventListener("click", loadCitation);
